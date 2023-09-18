e97927f1ff08a7662525239a358ffe1f1caf4fe2
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-09-17 19:20:46 -0700

    added question array, linked css and js to html

---
+++ Assets/script.js
@@ -0,0 +1,92 @@
+var questions = [
+    {
+        id: 0,
+        question: "What is the result of 'C R A B'.split(' ')?",
+        a: "crab",
+        b: "['C', 'R', 'A', 'B']",
+        c: "CRAB",
+        d: "['c', 'r', 'a', 'b']",
+        correct: "c"
+    },
+    {
+        id: 1,
+        question: "Which line of code adds the attribute 'display: none' to a div?",
+        a: "document.querySelector('div').addAttribute('display', 'none')",
+        b: "document.querySelector('div').setAttribute('display: none')",
+        c: "document.querySelector('div').addAttribute('display: none')",
+        d: "document.querySelector('div').setAttribute('display', 'none')",
+        correct: "d"
+    },
+    {
+        id: 2,
+        question: "Which of the following methods joins all elements of an array into a string?",
+        a: "Array.prototype.concat()",
+        b: "Array.prototype.join()",
+        c: "Array.prototype.map()",
+        d: "Array.prototype.splice()",
+        correct: "b"
+    },
+    {
+        id: 3,
+        question: "Which of the following choices demonstrates the correct syntax of a for loop?",
+        a: "for (i=0; i<array.length; i++)",
+        b: "for (i=0, i<array.length, i++)",
+        c: "for (i=0; i=array.length; i++)",
+        d: "for (i=0; i<array.length; i+1)",
+        correct: "a"
+    },
+    {
+        id: 4,
+        question: "Which of the following is NOT a valid String method?",
+        a: "String.length",
+        b: "String.replace()",
+        c: "String.reverse()",
+        d: "String.toUpperCase()",
+        correct: "c"
+    },
+    {
+        id: 5,
+        question: "Write the following phrase as an if condition: 'if color is not red...'",
+        a: "if (color NOT== 'red')",
+        b: "if (color ! 'red')",
+        c: "if (color == !('red'))",
+        d: "if (color !== 'red')",
+        correct: "d"
+    },
+    {
+        id: 6,
+        question: "Which line of code adds an event listener that triggers when an element is clicked?",
+        a: "elem.addEventListener('clicked', function)",
+        b: "elem.addEventListener('click', function)",
+        c: "elem.addEventTrigger('clicked', function)",
+        d: "elem.addEventTrigger('click', function)",
+        correct: "b"
+    },
+    {
+        id: 7,
+        question: "Which line of code selects a div with an id of 'password'?",
+        a: "document.querySelector('#password')",
+        b: "document.querySelector('password')",
+        c: "document.querySelector('.password')",
+        d: "document.querySelector(':password')",
+        correct: "a"
+    },
+    {
+        id: 8,
+        question: "Which of the following choices demonstrates the correct syntax for writing a function?",
+        a: "function(params) ( )",
+        b: "function(params) [ ]",
+        c: "function(params) { }",
+        d: "function[params] ( )",
+        correct: "c"
+    },
+    {
+        id: 9,
+        question: "In an object with multiple keys, the keys need to be separated by...",
+        a: "semicolons (;)",
+        b: "dashes (-)",
+        c: "periods (.)",
+        d: "commas (,)",
+        correct: "d"
+    }
+];
